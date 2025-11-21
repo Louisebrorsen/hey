e525985660a690117bf53ae0f5829dec7dc7b8f6
Louisebrorsen <loubro01@easv365.dk>
2025-11-21 19:30:48 +0100

    finpudsning admin side

--- app/views/admin/admin.php
+++ app/views/admin/admin.php
@@ -3,44 +3,47 @@
 $tab = $data['tab'] ?? 'movies';
 $base = __DIR__ . '/';
 ?>
-
-<nav class="tabs">
-<a class="tablink <?= $tab === 'movies' ? 'is-active' : '' ?>" href="?page=admin&tab=movies">Film</a>
-
-<a class="tablink <?= $tab === 'rooms' ? 'is-active' : '' ?>" href="?page=admin&tab=rooms">Sale & sæder</a>
-
-<a class="tablink <?= $tab === 'showtimes' ? 'is-active' : '' ?>" href="?page=admin&tab=showtimes">Showtimes</a>
-
-<a class="tablink <?= $tab === 'allMovies' ? 'is-active' : '' ?>" href="?page=admin&tab=allMovies">Alle film</a></nav>
-
+ 
 <main class="admin-main container" style="padding:40px 20px;">
-<h1>Admin </h1>
+  <div class="admin-content">
+    <h1>Admin </h1>
 
+    <nav class="tabs">
+      <a class="tablink <?= $tab === 'movies' ? 'is-active' : '' ?>" href="?page=admin&tab=movies">Film</a>
 
-<div class="admin-content">
+      <a class="tablink <?= $tab === 'rooms' ? 'is-active' : '' ?>" href="?page=admin&tab=rooms">Sale & sæder</a>
+
+      <a class="tablink <?= $tab === 'showtimes' ? 'is-active' : '' ?>" href="?page=admin&tab=showtimes">Showtimes</a>
+
+      <a class="tablink <?= $tab === 'allMovies' ? 'is-active' : '' ?>" href="?page=admin&tab=allMovies">Alle film</a>
+    </nav>
+
 
     <?php if ($tab === 'movies'): ?>
-        <?php include $base . 'movieAdd.php'; ?>
+            <?php include $base . 'movieAdd.php'; ?>
 
-    <?php elseif ($tab === 'rooms'): ?>
-        <?php include $base . 'auditorium.php'; ?>
+        <?php elseif ($tab === 'rooms'): ?>
+            <?php include $base . 'auditorium.php'; ?>
 
-    <?php elseif ($tab === 'showtimes'): ?>
-        <?php include $base . 'screening.php'; ?>
+        <?php elseif ($tab === 'showtimes'): ?>
+            <?php include $base . 'screening.php'; ?>
 
-    <?php elseif ($tab === 'allMovies'): ?>
-        <?php include $base . 'moviesList.php'; ?>
+        <?php elseif ($tab === 'allMovies'): ?>
+            <?php include $base . 'moviesList.php'; ?>
 
-    <?php else: ?>
-        <p>Ukendt tab.</p>
-    <?php endif; ?>
+        <?php else: ?>
+            <p>Ukendt tab.</p>
+        <?php endif; ?>
+  </div>
+</main>
+   
 
-</div>
+
   
 
  
 
-</main>
+
 
 <!-- <div style="margin:12px 0; padding:12px; border-radius:10px; background:#103f2c; color:#d7ffef;">
     Din besked vises her (fx "Filmen er oprettet").
